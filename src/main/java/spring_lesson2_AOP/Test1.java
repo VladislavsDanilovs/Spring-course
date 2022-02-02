765fa0bc777a948a8952c49c13acfb592ee38044
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-02-02 10:01:01 +0200

    add lesson6 (Combining Poincuts)

--- src/main/java/spring_lesson2_AOP/Test1.java
+++ src/main/java/spring_lesson2_AOP/Test1.java
@@ -7,10 +7,13 @@ public class Test1 {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
         UniLibrary l1 = context.getBean("uniLibrary", UniLibrary.class);
-        Book book = context.getBean("book", Book.class);
-        l1.getBook(book);
+//        Book book = context.getBean("book", Book.class);
+        l1.getBook();
+        l1.returnMagazine();
+        l1.addBook();
+
 //        l1.returnBook();
-//        l1.getMagazine();
+
 
 
 //        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
--- src/main/java/spring_lesson2_AOP/UniLibrary.java
+++ src/main/java/spring_lesson2_AOP/UniLibrary.java
@@ -5,19 +5,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class UniLibrary extends AbstractLibrary {
 
-    public void getBook(Book book){
-        System.out.println("Take book from UniLibrary " +book.getName());
+    public void getBook(){
+        System.out.println("Take book from UniLibrary ");
     }
 
-//    public String returnBook(){
-//        System.out.println("returning book into UniLibrary");
-//        return "okey";
-//    }
+    public void returnBook(){
+        System.out.println("returning book into UniLibrary");
+    }
 
     public void getMagazine(){
-
         System.out.println("Take magazine from UniLibrary");
-
     }
+
+    public void returnMagazine(){
+        System.out.println("Returning magazine to UniLibrary");
+    }
+
+    public void addBook(){
+        System.out.println("Add book to UniLibrary ");
+    }
+
+    public void addMagazine(){
+        System.out.println("Add magazine to UniLibrary ");
+    }
+
 
 }
