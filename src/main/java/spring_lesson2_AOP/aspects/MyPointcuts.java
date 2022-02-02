e640fbc8fb297976e1002ed7bf9cdc33dbc31de8
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-02-02 14:14:35 +0200

    add lesson8 (@AfterReturning)

--- src/main/java/spring_lesson2_AOP/aspects/MyPointcuts.java
+++ src/main/java/spring_lesson2_AOP/aspects/MyPointcuts.java
@@ -3,6 +3,6 @@ package spring_lesson2_AOP.aspects;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class MyPointcuts {
-    @Pointcut("execution(* add*(..))")
+    @Pointcut("execution(* addd*(..))")
     public void allAddMethods(){}
 }
---
+++ src/main/java/spring_lesson2_AOP/aspects/UniversityLoggingAspect.java
@@ -0,0 +1,36 @@
+package spring_lesson2_AOP.aspects;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import spring_lesson2_AOP.Student;
+
+import java.util.List;
+
+@Component
+@Aspect
+public class UniversityLoggingAspect {
+
+    @Before("execution(* getStudents())")
+    public void beforeGetStudentsLoggingAdvice() {
+        System.out.println("beforeGetStudentsLoggingAdvice: Logging list of students before getStudents()");
+    }
+
+    @AfterReturning(pointcut = "execution(* getStudents())"
+            , returning = "students")
+    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
+        Student firstStudent = students.get(0);
+        String nameSurname = firstStudent.getNameSurname();
+        nameSurname = "Mr. " + nameSurname;
+        firstStudent.setNameSurname(nameSurname);
+
+        double avgGrade = firstStudent.getAvgGrade();
+        avgGrade = avgGrade + 1;
+        firstStudent.setAvgGrade(avgGrade);
+
+        System.out.println("afterReturningGetStudentsLoggingAdvice: Logging list of students after getStudents()");
+    }
+
+
+}
