d6284e2a6f3d529f23ef5efa13102e1cf21bf1ee
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-02-08 12:48:03 +0200

    add lesson 16, MVC forms

--- spring_course_mvc/src/main/java/com/vladdanilov/spring/mvc/MyController.java
+++ spring_course_mvc/src/main/java/com/vladdanilov/spring/mvc/MyController.java
@@ -1,13 +1,65 @@
 package com.vladdanilov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
-    public String showMyView(){
+    public String showMyView() {
         return "myView";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(Model model){
+
+//        Employee emp = new Employee();
+//        emp.setName("Viktor");
+//        emp.setSurname("King");
+//        emp.setSalary(250);
+
+        model.addAttribute("employee", new Employee());
+        return "ask-emp-details-view";
+    }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(){
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model){
+//
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. "+ empName;
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("description", " -student");
+//
+//        return "show-emp-details-view";
+//    }
+
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@ModelAttribute("employee") Employee emp){
+
+            String name = emp.getName();
+            emp.setName("Mr " + name);
+
+            String surname = emp.getSurname();
+            emp.setSurname(surname + "!");
+
+            int salary = emp.getSalary();
+            emp.setSalary(salary*10);
+
+//        empName = "Mr. "+ empName;
+//        model.addAttribute("nameAttribute", empName);
+
+        return "show-emp-details-view";
     }
 }
---
+++ spring_course_mvc/src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2> Dear Employee, you are welcome! </h2>
+<br>
+<br>
+<br>
+
+
+<! –– Your name: ${param.employeeName} ––>
+
+Your name: ${employee.name}
+<br>
+Your surname: ${employee.surname}
+<br>
+Your salary: ${employee.salary}
+
+</body>
+</html>
