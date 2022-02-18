f136b3c1f2e8485724d9d733ec72685511571cbe
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-02-18 13:17:56 +0200

    add spring data jpa lesson

---
+++ spring_data_jpa/src/main/java/com/vladdanilov/spring/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.vladdanilov.spring.springboot.spring_data_jpa.dao;
+
+import com.vladdanilov.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+//First generic is entity type, second is type of primary key, in our case int id, so its Integer
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>  {
+    public List<Employee> findAllByName(String name);
+
+}
---
+++ spring_data_jpa/src/main/java/com/vladdanilov/spring/springboot/spring_data_jpa/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.vladdanilov.spring.springboot.spring_data_jpa.service;
+
+import com.vladdanilov.spring.springboot.spring_data_jpa.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+
+    public List<Employee> finaAllByName(String name);
+}
+
